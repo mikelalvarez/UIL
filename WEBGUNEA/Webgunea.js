ad2058a2337bfa8bb7aaaa109dd9d7d11b238279
mikelalvarez <45491909+mikelalvarez@users.noreply.github.com>
2018-11-30 16:43:22 +0100

    Add files via upload

---
+++ WEBGUNEA/Webgunea.js
@@ -0,0 +1,20 @@
+//file : Webgunea.js
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+if (window.pageYOffset >= sticky) {
+	navbar.classList.add("sticky")
+} else {
+	navbar.classList.remove("sticky");
+}
+}
+
+function apaegunon(name){
+    alert('Apa, egun on ' + 'Mikel');
+}
+
+apaegunon('Webgunea');
