49c6b052d76842a1eba740b4ec64f54243eb3287
ik013025mh <ik013025mh@H013025.NET>
2018-12-12 13:55:14 +0100

    !!

--- JavaScript/4/js.js
+++ JavaScript/4/js.js
@@ -1,3 +1,12 @@
-function kutxatxoak() {
-alert(document.querySelector('.messageCheckbox').checked);
+var a = ("", "", "")
+function kutxatxoak()
+{
+  if (kutxa1.checked)
+  {
+    alert("KARKARKAR");
+  }
+  else if (kutxa2.checked)
+  {
+    alert("KORKORKOR")
+  }
 }
