bb1e584feb02152dc71a50d0ddc1347483cbb724
ik013025mh <ik013025mh@H013025.NET>
2018-12-18 09:45:31 +0100

    !

--- JavaScript/jolasa/jolasa.js
+++ JavaScript/jolasa/jolasa.js
@@ -4,14 +4,8 @@ var markagailuJ1, markagailuJ2;
 var txanda = ["J1", "J2"];
 var kokatuta =[];
 var k = 12;
-var bist = "";
-
-/*function desgaitu(){
-var gelaxkak = document.getElementsByTagName('img');
-	for (var i=0; i < gelaxkak.length; i++){
-		gelaxkak[i].onclick = null;
-	}
-}*/
+var biratua1 = "";
+var biratua2 = "";
 
 function hasiJolasa(){
 	markagailuJ1 = 0;
@@ -36,8 +30,8 @@ function biratu(elemnt){
 	elemnt.src = "irudiak/" + kokatuta[elemnt.id] + ".png";
 	document.getElementById("elementua").innerHTML = kokatuta[elemnt.id];
 
-	if (bist=="") {
-		bist = kokatuta[element.id]
+	if (biratua1=="") {
+		biratua1= kokatuta[element.id]
 	} else {
 
 	}
